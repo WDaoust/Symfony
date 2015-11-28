d515a70c3dfa64a39f69a53c536c9ccd0f680445
WDaoust <daoust@et.esiea.fr>
2015-11-28 19:32:52 +0100

    fix view in advertController.php et routing.yml

--- src/WD/PlatformBundle/Controller/AdvertController.php
+++ src/WD/PlatformBundle/Controller/AdvertController.php
@@ -63,9 +63,22 @@ class AdvertController extends Controller
   }
   
 	public function viewAction($id){
-		$advert = new Advert;
-    $advert->setContent("Recherche développeur Symfony2.");
-	
+	// On récupère le repository
+    $repository = $this->getDoctrine()
+      ->getManager()
+      ->getRepository('WDPlatformBundle:Advert')
+    ;
+
+    // On récupère l'entité correspondante à l'id $id
+    $advert = $repository->find($id);
+
+    // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+    // ou null si l'id $id  n'existe pas, d'où ce if :
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    // Le render ne change pas, on passait avant un tableau, maintenant un objet
     return $this->render('WDPlatformBundle:Advert:view.html.twig', array(
       'advert' => $advert
     ));
